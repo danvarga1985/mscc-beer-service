1e2af96c14105eef32b185de85f8af92e0b7351b
danvarga1985 <danvarga1985@gmail.com>
2020-12-28 13:47:48 +0100

    Adding BeerRepository

---
+++ src/main/java/com/danvarga/msscbeerservice/repositories/BeerRepository.java
@@ -0,0 +1,11 @@
+package com.danvarga.msscbeerservice.repositories;
+
+import com.danvarga.msscbeerservice.domain.Beer;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface BeerRepository extends PagingAndSortingRepository<Beer, UUID> {
+}
