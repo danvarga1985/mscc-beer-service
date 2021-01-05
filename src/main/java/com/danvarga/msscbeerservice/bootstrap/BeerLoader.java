bae4f0527b889706d533d2efc3311486dddd1d08
danvarga1985 <danvarga1985@gmail.com>
2021-01-05 13:21:06 +0100

    Configuring MySql

--- src/main/java/com/danvarga/msscbeerservice/bootstrap/BeerLoader.java
+++ src/main/java/com/danvarga/msscbeerservice/bootstrap/BeerLoader.java
@@ -2,34 +2,27 @@ package com.danvarga.msscbeerservice.bootstrap;
 
 import com.danvarga.msscbeerservice.domain.Beer;
 import com.danvarga.msscbeerservice.repositories.BeerRepository;
+import com.danvarga.msscbeerservice.web.model.BeerStyleEnum;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.util.UUID;
 
 /*
- Cannot set fix ids (always overwritten with random ones), and they need to match the ids in the
- beer-inventory-service... -> data.sql used instead.
  TODO: beer-inventory-service bootstrap has no problem setting ids - implement BaseEntity here to check if that helps.
 */
 
-//@Component
+@Component
+@RequiredArgsConstructor
 // The CommandLineRunner interface ensures that the 'run' method will run every time the Spring context starts.
 public class BeerLoader implements CommandLineRunner {
 
     public static final String BEER_1_UPC = "0631234200036";
     public static final String BEER_2_UPC = "0631234500037";
     public static final String BEER_3_UPC = "0631334260016";
-    public static final UUID BEER_1_UUID = UUID.fromString("0a818933-087d-47f2-ad83-2f986ed087eb");
-    public static final UUID BEER_2_UUID = UUID.fromString("a712d914-61ea-4623-8bd0-32c0f6545bfd");
-    public static final UUID BEER_3_UUID = UUID.fromString("026cc3c8-3a0c-4083-a05b-e908048c1b08");
 
     private final BeerRepository beerRepository;
-
-    public BeerLoader(BeerRepository beerRepository) {
-        this.beerRepository = beerRepository;
-    }
 
     @Override
     public void run(String... args) throws Exception {
@@ -40,9 +33,8 @@ public class BeerLoader implements CommandLineRunner {
         if (beerRepository.count() == 0) {
 
             beerRepository.save(Beer.builder()
-                    .id(BEER_1_UUID)
                     .beerName("Mango Bobs")
-                    .beerStyle("IPA")
+                    .beerStyle(BeerStyleEnum.IPA.name())
                     .quantityToBrew(200)
                     .minOnHand(10)
                     .upc(BEER_1_UPC)
@@ -50,9 +42,8 @@ public class BeerLoader implements CommandLineRunner {
                     .build());
 
             beerRepository.save(Beer.builder()
-                    .id(BEER_2_UUID)
                     .beerName("Galaxy Cat")
-                    .beerStyle("PALE_ALE")
+                    .beerStyle(BeerStyleEnum.PALE_ALE.name())
                     .quantityToBrew(200)
                     .minOnHand(10)
                     .upc(BEER_2_UPC)
@@ -60,9 +51,8 @@ public class BeerLoader implements CommandLineRunner {
                     .build());
 
             beerRepository.save(Beer.builder()
-                    .id(BEER_3_UUID)
-                    .beerName("Duff")
-                    .beerStyle("LAGER")
+                    .beerName("Pinball Porter")
+                    .beerStyle(BeerStyleEnum.PORTER.name())
                     .quantityToBrew(200)
                     .minOnHand(13)
                     .upc(BEER_3_UPC)
