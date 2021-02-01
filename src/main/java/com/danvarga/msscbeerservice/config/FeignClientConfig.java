05e9a07f5d3a5fe043f5c151c49b594616b4c02a
danvarga1985 <danvarga1985@gmail.com>
2021-02-01 15:56:03 +0100

    Configuring Feign Client for Basic Auth

---
+++ src/main/java/com/danvarga/msscbeerservice/config/FeignClientConfig.java
@@ -0,0 +1,18 @@
+package com.danvarga.msscbeerservice.config;
+
+import feign.auth.BasicAuthRequestInterceptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FeignClientConfig {
+
+    // Feign Basic Auth config.
+    @Bean
+    public BasicAuthRequestInterceptor basicAuthRequestInterceptor(@Value("${danvarga.brewery.inventory-user}") String inventoryUser,
+                                                                   @Value("${danvarga.brewery.inventory-password}") String inventoryPassword) {
+
+        return new BasicAuthRequestInterceptor(inventoryUser, inventoryPassword);
+    }
+}
